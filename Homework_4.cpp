f6e044260d0ec215ab9cefa2662fedb58f7e2324
YUYEON91 <cybott2002@gmail.com>
2025-08-26 22:51:12 +0900

    과제 수정

--- Homework_4.cpp
+++ Homework_4.cpp
@@ -18,45 +18,6 @@ public:
     }
 };
 
-// AlchemyWorkshop 클래스: 레시피 목록을 관리
-class AlchemyWorkshop
-{
-private:
-    vector<PotionRecipe> recipes;
-
-public:
-
-    // 모든 레시피 출력 메서드
-    void displayAllRecipes() const 
-    {
-        if (recipes.empty()) 
-        {
-            cout << "아직 등록된 레시피가 없습니다." << endl;
-            return;
-        }
-
-        cout << "\n--- [ 전체 레시피 목록 ] ---" << endl;
-        for (size_t i = 0; i < recipes.size(); ++i) 
-        {
-            cout << "- 물약 이름: " << recipes[i].potionName << endl;
-            cout << "  > 필요 재료: ";
-
-            // 재료 목록을 순회하며 출력
-            for (size_t j = 0; j < recipes[i].ingredients.size(); ++j) 
-            {
-                cout << recipes[i].ingredients[j];
-                // 마지막 재료가 아니면 쉼표로 구분
-                if (j < recipes[i].ingredients.size() - 1) 
-                {
-                    cout << ", ";
-                }
-            }
-            cout << endl;
-        }
-        cout << "---------------------------\n";
-    }
-};
-
 //도전기능 재고 관리
 class StockManager
 {
@@ -126,6 +87,53 @@ public:
 
     }
 };
+
+// AlchemyWorkshop 클래스: 레시피 목록을 관리
+class AlchemyWorkshop
+{
+private:
+    vector<PotionRecipe> recipes;
+    StockManager& stockManager; // StockManager
+
+public:
+    AlchemyWorkshop(StockManager& stockManager) : stockManager(stockManager) {}
+
+    // 모든 레시피 출력 메서드
+    void displayAllRecipes() const 
+    {
+        if (recipes.empty()) 
+        {
+            cout << "아직 등록된 레시피가 없습니다." << endl;
+            return;
+        }
+
+        cout << "\n--- [ 전체 레시피 목록 ] ---" << endl;
+        for (size_t i = 0; i < recipes.size(); ++i) 
+        {
+            cout << "- 물약 이름: " << recipes[i].potionName << endl;
+            cout << "  > 필요 재료: ";
+            // 재료 목록을 순회하며 출력
+            for (size_t j = 0; j < recipes[i].ingredients.size(); ++j) 
+            {
+                cout << recipes[i].ingredients[j];
+                // 마지막 재료가 아니면 쉼표로 구분
+                if (j < recipes[i].ingredients.size() - 1) 
+                {
+                    cout << ", ";
+                }
+            }
+            cout << endl;
+            int stock = stockManager.getStock(recipes[i].potionName);
+            cout << "[재고 확인] '" << recipes[i].potionName << "' 현재 재고 : " << stock << "개" << endl;
+        }
+        cout << "---------------------------\n";
+    }
+    void setRecipes(const vector<PotionRecipe>& newRecipes)
+    {
+        recipes = newRecipes;
+    }
+};
+
 
 // 도전기능 레시피 관리
 class RecipeManager
@@ -214,10 +222,11 @@ public:
 };
 
 
-int main() {
-    AlchemyWorkshop myWorkshop;
+int main() 
+{
     StockManager stockManager;
     RecipeManager recipeManager(stockManager);
+    AlchemyWorkshop myWorkshop(stockManager);
 
     while (true) 
     {
@@ -280,8 +289,6 @@ int main() {
             // 입력받은 재료가 하나 이상 있을 때만 레시피 추가
             if (!ingredients_input.empty()) {
                 recipeManager.addRecipe(name, ingredients_input);
-                int stock = stockManager.getStock(name);
-                cout << "[재고 확인]'" << name << "'현재 재고: " << stock << "개" << endl;
             }
             else 
             {
@@ -291,8 +298,8 @@ int main() {
         }
         else if (choice == 2) 
         {
+            myWorkshop.setRecipes(recipeManager.getAllRecipes());
             myWorkshop.displayAllRecipes();
-
         }
 
         // 물약 이름으로 레시피 찾기
